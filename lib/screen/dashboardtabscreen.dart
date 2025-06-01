6deb3dc134721a8157ac70e137a70ddca0f76087
unknown <manubarca921@gmail.com>
2025-06-01 13:57:17 +0545

    screen

---
+++ lib/screen/dashboardtabscreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DashboardTabScreen extends StatelessWidget {
+  const DashboardTabScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Dashboard Tab Screen', style: TextStyle(fontSize: 22)),
+    );
+  }
+}
---
+++ lib/screen/giftcardscreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GiftCardScreen extends StatelessWidget {
+  const GiftCardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Gift Card Screen', style: TextStyle(fontSize: 22)),
+    );
+  }
+}
